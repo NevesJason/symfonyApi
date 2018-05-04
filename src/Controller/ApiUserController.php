a1e2eb6d1a30c72da693458a47553b0fb9f68ae1
NevesJason <jason.neves@senalia.com>
2018-05-04 13:34:58 +0200

    v3

--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -1,21 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jneves
- * Date: 27/04/2018
- * Time: 14:42
- */
-namespace App\Controller;
 
+namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
-//use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Entity\User;
 use App\Repository\UserRepository;
 
 /**
- * @Route('/api',name="Api_controller")
+ * @Route("/api",name="Api_controller")
  */
 class ApiUserController  extends Controller
 {
@@ -24,7 +18,7 @@ class ApiUserController  extends Controller
      */
     public function index(Request $request)
     {
-        return new JsonResponse(['user' => $this->getUser()]);
+        return new JsonResponse(['token' => $this->getUser()]);
     }
 
 
